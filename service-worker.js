87f417724c7bc1aca487f7fea93af34c55c58b08
Antaresx101 <servitor.x101@gmail.com>
2025-07-18 02:47:46 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,33 @@
-const CACHE_NAME = 'warhammer-damage-calculator-v1';
-const urlsToCache = [
-    '/warhammer-damage-calculator/',
-    '/warhammer-damage-calculator/index.html',
-    '/warhammer-damage-calculator/icon.png'
-];
-
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
-});
+         event.waitUntil(
+             caches.open('dawn-v1').then(cache => {
+                 return cache.addAll([
+                     '/',
+                     '/index.html',
+                     '/icon.png',
+                     '/manifest.json',
+                     '/icons/inactive.png',
+                     '/icons/reroll_1s_hit.png',
+                     '/icons/reroll_hits.png',
+                     '/icons/fish_for_hits.png',
+                     '/icons/sustained_hits_1.png',
+                     '/icons/sustained_hits_2.png',
+                     '/icons/sustained_hits_3.png',
+                     '/icons/sustained_hits_D3.png',
+                     '/icons/lethal_hits.png',
+                     '/icons/reroll_1s_wounds.png',
+                     '/icons/reroll_wounds.png',
+                     '/icons/fish_for_wounds.png',
+                     '/icons/devastating_wounds.png'
+                 ]);
+             })
+         );
+     });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
-});
+     self.addEventListener('fetch', event => {
+         event.respondWith(
+             caches.match(event.request).then(response => {
+                 return response || fetch(event.request);
+             })
+         );
+     });
